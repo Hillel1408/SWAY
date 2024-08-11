eceda0f7add60ad14d866b977b7a0b0f9d15823e
Hillel1408 <etemax@bk.ru>
2024-08-11 11:05:56 +0300

    интро

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components";
+
+export default function Intro() {
+  return (
+    <div className="_container pb-[83px] pt-[120px]">
+      <div className="grid grid-cols-2">
+        <div className="max-w-[458px]">
+          <img src="img/logo.svg" alt="" width="250px" />
+          <h1 className="font-bold text-[36px] leading-[44px] text-black mt-[9px] mb-[14px]">
+            A MARKETING AGENCY
+          </h1>
+          <p className="text-[16px] leading-[19px] text-[#797979] mb-[18px]">
+            Were not just another influencer marketing agency. Were a team of
+            passionate creators, strategists, and innovators dedicated to
+            redefining the way brands connect with their audiences.
+          </p>
+          <Button text="GET IN TOUCH" className="text-white bg-black" />
+        </div>
+        <img src="img/img-1.png" alt="" width="" />
+      </div>
+    </div>
+  );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Button from "@/components/Button/Button";
+import Intro from "@/components/Intro/Intro";
 
-export { Header, Footer, Button };
+export { Header, Footer, Button, Intro };
