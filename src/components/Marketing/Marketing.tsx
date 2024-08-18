79d09498578149eb3cf3e4a26849f7c17438d096
Hillel1408 <etemax@bk.ru>
2024-08-18 20:29:20 +0300

    правки стилей

--- src/components/Marketing/Marketing.tsx
+++ src/components/Marketing/Marketing.tsx
@@ -62,41 +62,45 @@ export default function Marketing() {
     ];
 
     return (
-        <div className="bg-black pt-[60px] pb-[36px] overflow-hidden">
-            <div className="_container">
-                <h2 className="font-bold text-[36px] leading-[44px] text-white uppercase mb-[36px]">
-                    MARKETING REPORTS TO EQUIP YOUR TEAM <br /> WITH THE MOST RELEVANT INSIGHTS
-                </h2>
-                {loaded && instanceRef.current && (
-                    <div className="dots mb-[36px] flex gap-[11px]">
-                        {[...Array(instanceRef.current.track.details.slides.length - 2).keys()].map((idx) => {
-                            return (
-                                <button
-                                    key={idx}
-                                    onClick={() => {
-                                        instanceRef.current?.moveToIdx(idx);
-                                    }}
-                                    className={classNames(currentSlide === idx ? "bg-white" : "", "w-[11px] h-[11px] border border-white rounded-full")}
-                                ></button>
-                            );
-                        })}
-                    </div>
-                )}
-                <div ref={sliderRef} className="keen-slider !overflow-visible">
-                    {slides.map((item, index) => (
-                        <div key={index} className="keen-slider__slide">
-                            <div className="pt-[176px] bg-[#C4C4C4] mb-[45px]">
-                                <div className="absolute top-0 bottom-0 left-0 right-0">
-                                    <img src={`img/${item.image}`} alt="" className="object-cover h-full" />
-                                </div>
-                                <div className="bg-black py-[45px] px-[30px] mx-[30px] translate-y-[36px] flex flex-col gap-[3px]">
-                                    <p className="font-bold text-[26px] leading-[33px] text-white">{item.name}</p>
-                                    <p className="font-bold text-[26px] leading-[33px] text-white">{item.description}</p>
-                                    <p className="font-bold text-[26px] leading-[33px] text-white">{item.email}</p>
-                                </div>
-                            </div>
+        <div className="overflow-hidden pb-[90px]">
+            <div className="bg-black pt-[60px]">
+                <div className="_container relative min-h-[464px]">
+                    <h2 className="font-bold text-[36px] leading-[44px] text-white uppercase mb-[36px]">
+                        MARKETING REPORTS TO EQUIP YOUR TEAM <br /> WITH THE MOST RELEVANT INSIGHTS
+                    </h2>
+                    {loaded && instanceRef.current && (
+                        <div className="dots mb-[36px] flex gap-[11px]">
+                            {[...Array(instanceRef.current.track.details.slides.length - 2).keys()].map((idx) => {
+                                return (
+                                    <button
+                                        key={idx}
+                                        onClick={() => {
+                                            instanceRef.current?.moveToIdx(idx);
+                                        }}
+                                        className={classNames(currentSlide === idx ? "bg-white" : "", "w-[11px] h-[11px] border border-white rounded-full")}
+                                    ></button>
+                                );
+                            })}
                         </div>
-                    ))}
+                    )}
+                    <div className="absolute left-0 right-0">
+                        <div ref={sliderRef} className="keen-slider !overflow-visible">
+                            {slides.map((item, index) => (
+                                <div key={index} className="keen-slider__slide">
+                                    <div className="pt-[176px] pb-[45px]">
+                                        <div className="absolute top-0 bottom-0 left-0 right-0 h-[calc(100%-70px)]">
+                                            <img src={`img/${item.image}`} alt="" className="object-cover h-full" />
+                                        </div>
+                                        <div className="bg-black py-[45px] px-[30px] mx-[30px] translate-y-[36px] flex flex-col gap-[3px]">
+                                            <p className="font-bold text-[26px] leading-[33px] text-white">{item.name}</p>
+                                            <p className="font-bold text-[26px] leading-[33px] text-white">{item.description}</p>
+                                            <p className="font-bold text-[26px] leading-[33px] text-white">{item.email}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
