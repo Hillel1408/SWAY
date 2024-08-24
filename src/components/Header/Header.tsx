530105b26644c37711d555a2da5478a99b89d967
Hillel1408 <etemax@bk.ru>
2024-08-24 15:57:47 +0300

    header адаптив

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,37 @@
+"use client";
+
 import { Button } from "@/components";
+import classNames from "classnames";
+import { useState } from "react";
 
 export default function Header() {
+    const [active, setActive] = useState(false);
     const list = [{ text: "Our work" }, { text: "about us" }, { text: "REACH MAX+" }, { text: "PROCESS" }, { text: "GUARANTEE" }, { text: "OUR TEAM" }];
 
     return (
-        <header className="_container flex items-center py-[15px] left-0 right-0 md:fixed md:bg-white md:z-10 md:bottom-0 md:top-0 md:left-0 md:right-0 md:flex-col md:items-start">
-            <img src="img/logo.svg" alt="" />
-            <ul className="flex gap-[30px] text-[14px] text-[#797979] leading-[17px] uppercase ml-[55px] flex-wrap md:ml-0 md:flex-col">
-                {list.map((item, index) => (
-                    <li key={index}>{item.text}</li>
-                ))}
-            </ul>
-            <Button text="GET IN TOUCH" className="text-black bg-white ml-auto md:ml-0" />
+        <header className="_container py-[15px]">
+            <div
+                className={classNames(
+                    "flex items-center left-0 right-0 md:fixed md:bg-white md:z-10 md:bottom-0 md:top-0 md:left-0 md:right-0 md:flex-col md:items-start duration-200 md:_container md:py-[15px] md:overflow-auto",
+                    active ? "md:opacity-100 md:visible" : "md:opacity-0 md:invisible",
+                )}
+            >
+                <img src="img/logo.svg" alt="" className="md:mb-10" />
+                <button className="hidden md:block absolute right-[18px] top-[18px]" onClick={() => setActive(false)}>
+                    <img src="img/nav-btn.svg" alt="" />
+                </button>
+                <ul className="flex gap-[30px] text-[14px] text-[#797979] leading-[17px] uppercase ml-[55px] flex-wrap md:ml-0 md:flex-col md:gap-[50px]">
+                    {list.map((item, index) => (
+                        <li key={index}>{item.text}</li>
+                    ))}
+                </ul>
+                <div className="md:pt-[46px] md:pb-[178px] md:pr-[108px] md:pl-5 md:bg-black md:mt-10 ml-auto md:-ml-[17px]">
+                    <Button text="GET IN TOUCH" className="text-black bg-white" />
+                </div>
+            </div>
+            <button className="hidden md:block ml-auto" onClick={() => setActive(true)}>
+                <img src="img/btn-nav-open.svg" alt="" />
+            </button>
         </header>
     );
 }
